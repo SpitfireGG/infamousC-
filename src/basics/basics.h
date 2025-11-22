e0119c27d862c8438bd14e39d2420153522d1b80
SpitfireGG <raambasnet02@gmail.com>
2025-11-22 23:57:05 +0545

    add: classic Mersenne Twister

--- src/basics/basics.h
+++ src/basics/basics.h
@@ -4,26 +4,38 @@
 #include <cstring>
 #include <iostream>
 
-namespace First
-
-{
-    namespace Inner
-    {
-        void foo();
-    }
-    bool hello(char* str);
+namespace First {
+namespace Inner {
+void foo();
+}
+bool hello(char *str);
 } // namespace First
 
-namespace Second
-{
-    void foo();
-    void bar();
+namespace Second {
+void foo();
+void bar();
 } // namespace Second
 
-namespace Basics
-{
-    void fooji(void);
-    int add(int* a, int* b);
+namespace Basics {
+void fooji(void);
+int add(int *a, int *b);
 } // namespace Basics
+
+namespace NumberGen {
+void SeedRPNGs(unsigned int seed);
+unsigned int RPNGs();
+void MersenneTwister();
+void MoreRandomizedMersenneTwister();
+void DiceRoll();
+} // namespace NumberGen
+
+namespace ASCII_VAL {
+void AsciiVal();
+} // namespace ASCII_VAL
+
+namespace Pattern {
+void patternStr();
+void patternRev();
+} // namespace Pattern
 
 #endif
